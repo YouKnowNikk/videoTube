e7d07467a0360b054c78982f719583b6d2e7e5a5
Nikesh Gupta <nikeshgupta130@gmail.com>
2024-01-20 21:46:08 +0530

    intoduced multer and cloudinary . setup multer upload as middleware

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -49,7 +49,7 @@ userSchema.pre("save", async function (next) {
         return next()
     }
     else {
-        this.password = bcrypt.hash(this.password, 10)
+        this.password = await bcrypt.hash(this.password, 10)
         next()
     }
 })
